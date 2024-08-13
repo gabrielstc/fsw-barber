b91393dac4a392c3b04de4e8798fcbf4a6a6678b
Gabriel Strieder <gabriel.strieder@corebiz.ag>
2024-08-12 23:25:56 -0300

    feat: header and images content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { Button } from "./_components/ui/button";
+"use client"
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-red-500 mb-5 bg-red-300">Welcome to Next.js!</h1> 
-      <Button>Click me</Button>
-    </div>
-  );
+  return <div>{/* header */}</div>
 }
