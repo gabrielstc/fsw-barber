671acf4695bc05bad32769f4675098db79a5a5ea
Gabriel Strieder <gabriel.strieder@corebiz.ag>
2024-08-13 01:44:40 -0300

    feat: add booking-item component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent } from "./ui/card"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Badge } from "./ui/badge"
+
+const BookingItem = () => {
+  return (
+    <>
+      <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+        Agendamentos
+      </h2>
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          {/* esquerda */}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge color="primary" className="w-fit">
+              Confirmado
+            </Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage
+                  src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png"
+                  alt="Avatar"
+                />
+              </Avatar>
+              <p className="text-sm">München Barbearia</p>
+            </div>
+          </div>
+
+          {/* direita */}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">05</p>
+            <p className="text-sm">20:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,19 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
-  console.log(barbershops)
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+
   return (
     <div>
       {/* header */}
@@ -30,6 +35,21 @@ const Home = async () => {
           </Button>
         </div>
 
+        {/* busca rapida */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
         {/* banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
@@ -41,37 +61,7 @@ const Home = async () => {
         </div>
 
         {/* agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge color="primary" className="w-fit">
-                Confirmado
-              </Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage
-                    src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png"
-                    alt="Avatar"
-                  />
-                </Avatar>
-                <p className="text-sm">München Barbearia</p>
-              </div>
-            </div>
-
-            {/* direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
@@ -81,7 +71,27 @@ const Home = async () => {
             <BarberShopItem barbershop={barbershop} key={barbershop.id} />
           ))}
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarberShopItem barbershop={barbershop} key={barbershop.id} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2024 Copyright{" "}
+              <span className="font-bold">Dev Gabriel Strieder</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
