570c3776a35511f91466a7ba9544ed096ef0ac06
Gabriel Strieder <gabriel.strieder@corebiz.ag>
2024-08-12 23:50:23 -0300

    feat: add Header component and banner

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
 
 const Header = () => {
   return (
     <Card>
-      <CardContent></CardContent>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/logo.png" alt="Logo" width={200} height={100} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
     </Card>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
-"use client"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <div>{/* header */}</div>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gabriel</h2>
+        <p>Segunda-feira, 05 de agosto </p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende com os melhores"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
