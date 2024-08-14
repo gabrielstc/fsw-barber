c446a210081d2f5194285818565493957c90f1a3
Gabriel Strieder <gabriel.strieder@corebiz.ag>
2024-08-13 23:04:36 -0300

    feat: add PhoneItem component and Footer to layout

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
@@ -25,6 +26,7 @@ const BarberShopPage = async ({ params }: BarberShopPageProps) => {
   if (!barbershop) {
     return notFound()
   }
+
   return (
     <div>
       {/* imagem */}
@@ -56,7 +58,7 @@ const BarberShopPage = async ({ params }: BarberShopPageProps) => {
         </Button>
       </div>
 
-      {/* endereco */}
+      {/* titulo */}
       <div className="border-b border-solid p-5">
         <h1 className="mb-6 text-xl font-bold">{barbershop.name}</h1>
         <div className="mb-2 flex items-center gap-1">
@@ -75,14 +77,21 @@ const BarberShopPage = async ({ params }: BarberShopPageProps) => {
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre Nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
-
-      <div className="space-y-3 p-5">
+      {/* serviços */}
+      <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
         <div className="space-y-3">
           {barbershop.services.map((service) => (
             <ServiceItem key={service.id} service={service} />
           ))}
         </div>
+      </div>
+
+      {/* contato */}
+      <div className="space-y-3 p-5">
+        {barbershop.phones.map((phone) => (
+          <PhoneItem key={phone} phone={phone} />
+        ))}
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
@@ -81,17 +80,6 @@ const Home = async () => {
           ))}
         </div>
       </div>
-
-      <footer>
-        <Card>
-          <CardContent className="px-5 py-6">
-            <p className="text-sm text-gray-400">
-              © 2024 Copyright{" "}
-              <span className="font-bold">Dev Gabriel Strieder</span>
-            </p>
-          </CardContent>
-        </Card>
-      </footer>
     </div>
   )
 }
