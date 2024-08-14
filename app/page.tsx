87463d4443112e9f89f6c9a2e9c4a26fa4f99283
Gabriel Strieder <gabriel.strieder@corebiz.ag>
2024-08-14 02:09:32 -0300

    feat: ensure user can search for barbershops

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
+import Search from "./_components/search"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
@@ -27,11 +26,8 @@ const Home = async () => {
         <p>Terça-feira, 13 de agosto </p>
 
         {/* busca */}
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca..." />
-          <Button size="icon">
-            <SearchIcon />
-          </Button>
+        <div className="mt-6">
+          <Search />
         </div>
 
         {/* busca rapida */}
