f37a7f5f482e44d3ac9bfa3cb86081d93f06ff53
Gabriel Strieder <gabriel.strieder@corebiz.ag>
2024-10-23 19:26:14 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -47,8 +49,19 @@ const Home = async () => {
 
       <div className="p-5">
         {/* texto */}
-        <h2 className="text-xl font-bold">Olá, Gabriel</h2>
-        <p>Terça-feira, 13 de agosto </p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session?.user?.name : "bem-vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}{" "}
+          </span>
+
+          {" de "}
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}{" "}
+          </span>
+        </p>
 
         {/* busca */}
         <div className="mt-6">
